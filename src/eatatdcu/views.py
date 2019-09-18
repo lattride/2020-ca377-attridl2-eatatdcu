6bcb4f357c13584c0b73196f2d969c1a170322f8
Jennifer Foster <jfoster@computing.dcu.ie>
2019-09-18 10:59:09 +0100

    view

---
+++ src/eatatdcu/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+   context = {}
+   return render(request,'eatatdcu/index.html',context)
+
+# TODO add a restaurants function which loads the restaurants template
