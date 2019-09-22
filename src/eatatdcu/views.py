b36beb2ccd37b6b1103c45bb27e929d96bec9c3f
Jennifer Foster <Jennifer.Foster@dcu.ie>
2019-09-22 12:51:11 +0100

    A0 complete

--- src/eatatdcu/views.py
+++ src/eatatdcu/views.py
@@ -6,3 +6,6 @@ def index(request):
    return render(request,'eatatdcu/index.html',context)
 
 # TODO add a restaurants function which loads the restaurants.html template
+def restaurants(request):
+   context = {}
+   return render(request,'eatatdcu/restaurants.html',context)
