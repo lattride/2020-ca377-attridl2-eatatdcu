435af7cea542d535cff36c074408fdbb8192e935
Luke Attride <luke.attride2@mail.dcu.ie>
2019-09-28 17:15:06 +0100

    Update urls.py

--- src/eatatdcu/urls.py
+++ src/eatatdcu/urls.py
@@ -6,4 +6,5 @@ app_name = 'eatatdcu'
 #TODO add the path for the restaurants url
 urlpatterns = [
    path('',views.index,name='index'),
+   path('restaurants',views.restaurants,name='restaurants')
 ]
