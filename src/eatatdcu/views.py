e317c18ad6d2eda73ee24fb4222822c7c3d740a2
Luke Attride <luke.attride2@mail.dcu.ie>
2019-09-28 17:31:37 +0100

    Update views.py

--- src/eatatdcu/views.py
+++ src/eatatdcu/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
- def index(request):
-    context = {}
-    return render(request,'eatatdcu/index.html',context)
- def restaurants(request):
-    context = {}
-    return render(request, 'eatatdcu/restaurants.html',context)
+def index(request):
+   context = {}
+   return render(request,'eatatdcu/index.html',context)
+def restaurants(request):
+   context = {}
+   return render(request, 'eatatdcu/restaurants.html',context)
 
 # TODO add a restaurants function which loads the restaurants.html template
