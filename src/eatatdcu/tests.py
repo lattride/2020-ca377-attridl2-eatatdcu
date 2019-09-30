1c81697881f9732e031722695e839cca5acc2ecc
Jennifer Foster <Jennifer.Foster@dcu.ie>
2019-09-30 21:17:50 +0100

    a1 solution

--- src/eatatdcu/tests.py
+++ src/eatatdcu/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
+from eatatdcu.models import Campus,Restaurant
+from datetime import time
 
 class A0Tests(TestCase):
 
@@ -11,9 +13,65 @@ class A0Tests(TestCase):
       self.assertEqual(response.status_code, 200)
       self.assertContains(response, "Eat at DCU!")
 
-    def test_restaurants(self):
-      """
-      The restaurants page loads 
-      """
-      response = self.client.get(reverse('eatatdcu:restaurants'))
-      self.assertEqual(response.status_code, 200)
+    #def test_restaurants(self):
+    #  """
+    #  The restaurants page loads 
+    #  """
+    #  response = self.client.get(reverse('eatatdcu:restaurants'))
+    #  self.assertEqual(response.status_code, 200)
+
+class A1Tests(TestCase):
+
+    def setup(self):
+        """
+        Sets up a test database 
+        """
+        campus1 = Campus(1,'test campus')
+        campus2 = Campus(2,'another test campus')
+        campus3 = Campus(3,'yet another test campus')
+        rest1 = Restaurant(1, 'test r1','student centre',1,time(hour=8),time(hour=17),750)
+        rest2 = Restaurant(2, 'test r2','ballymun road',1,time(hour=9),time(hour=18),250)
+        rest3 = Restaurant(3, 'test r3','in library',1,time(hour=10),time(hour=17),300)
+        rest4 = Restaurant(4, 'test r4','beside entrance',3,time(hour=10),time(hour=16),200)
+        campus1.save()
+        campus2.save()
+        campus3.save()
+        rest1.save()
+        rest2.save()
+        rest3.save()
+        rest4.save()
+
+    def test_rest_retrieval(self):
+        """ 
+        Test retrieval of restaurants for a campus
+        """
+        self.setup()
+        response = self.client.get(reverse('eatatdcu:restaurants'),{'campus':'test campus'})
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,'r1')
+        self.assertContains(response,'r2')
+        self.assertContains(response,'r3')
+
+    def test_rest_retrieval_case(self):
+        """ Test retrieval of restaurants for a campus (case-insensitive)"""
+        self.setup()
+        response = self.client.get(reverse('eatatdcu:restaurants'),{'campus':'Test Campus'})
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,'r1')
+        self.assertContains(response,'r2')
+        self.assertContains(response,'r3')
+    
+    def test_rest_empty_retrieval(self):
+        """ Test empty retrieval of restaurants for a campus """
+        self.setup()
+        response = self.client.get(reverse('eatatdcu:restaurants'),{'campus':'another test campus'})
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,'No restaurants found')
+
+    def test_rest_retrieval_invalid(self):
+        """ Test retrieval of restaurants for an invalid campus """
+        self.setup()
+        response = self.client.get(reverse('eatatdcu:restaurants'),{'campus':'invalid campus'})
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,'No such campus')
+
--- src/eatatdcu/views.py
+++ src/eatatdcu/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Restaurant,Campus
 
 def index(request):
    context = {}
    return render(request,'eatatdcu/index.html',context)
 
-# TODO add a restaurants function which loads the restaurants.html template
 def restaurants(request):
    context = {}
+
+   #TODO get the campus name from the request
+   campus_name = request.GET.get('campus').lower()
+
+   #TODO retrieve the campus id from the db given this campus name
+   #TODO find all restaurants for that campus 
+   #
+   try:
+       campus = Campus.objects.get(name=campus_name)
+       restaurants = Restaurant.objects.filter(campus_id=campus)
+       context = {'restaurants':restaurants}
+   except Restaurant.DoesNotExist:
+       context = {'error':'No such restaurant'}
+   except Campus.DoesNotExist:
+       context = {'error':'No such campus'}
+
    return render(request,'eatatdcu/restaurants.html',context)
