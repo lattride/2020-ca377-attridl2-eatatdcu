1f2d7f1a594d984de2da319bf1650eb928bb86bf
Luke Attride <luke.attride2@mail.dcu.ie>
2019-10-09 14:05:16 +0100

    A1 complete

--- src/eatatdcu/views.py
+++ src/eatatdcu/views.py
@@ -5,12 +5,20 @@ from .models import Restaurant,Campus
 def index(request):
    context = {}
    return render(request,'eatatdcu/index.html',context)
-def restaurants(request):
-   context = {}
-   return render(request, 'eatatdcu/restaurants.html',context)
 
 def restaurants(request):
-   context = {}
+   campus_name = request.GET.get('campus').lower()
+   try:
+      campus = Campus.objects.get(name=campus_name)
+      restaurants = Restaurant.objects.filter(campus_id=campus)
+      if len(restaurants) == 0:
+         context = {'error':'No restaurants found'}
+      else:
+         context = {'DCU':restaurants}
+   except Campus.DoesNotExist:
+      context = {'error':'No such campus'}
+      return render(request, 'eatatdcu/restaurants.html',context)
+
 
    #TODO get the campus name from the request
    #TODO retrieve the campus id from the db given this campus name
