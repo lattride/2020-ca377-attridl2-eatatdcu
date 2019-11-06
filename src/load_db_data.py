538235a620fd2fa273dc4d698d8d848543df5e84
Luke Attride <luke.attride2@mail.dcu.ie>
2019-11-06 11:13:36 +0000

    a3 complete

--- src/load_db_data.py
+++ src/load_db_data.py
@@ -18,7 +18,7 @@ with open('../data/restaurant.csv') as re:
        restaurant = Restaurant(int(row[0]),row[1],row[2],int(row[3]),
                time(hour=int(opening[0]),minute=int(opening[1])),
                time(hour=int(closing[0]),minute=int(closing[1])),
-               int(row[6]),(row[7]),
+               int(row[6]),row[7],row[8],
                time(hour=int(opening_wknd[0]),minute=int(opening_wknd[1])),
                time(hour=int(closing_wknd[0]),minute=int(closing_wknd[1])),
                )
